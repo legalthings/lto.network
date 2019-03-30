8fa4307acd7a3bf8d5ac67563f3eb50d32d1642a
Sarfaraaz <sarfaraaz.jahangier@gmail.com>
2019-03-30 13:01:29 +0100

    Host assets ourselves, fix document links, some text & deps

--- js/custom/products.js
+++ js/custom/products.js
@@ -1,31 +1,31 @@
 const products = [
   {
     "title": "Workflow Engine",
-    "description": "<p>The workflow engine facilitates running processes and holding the state.</p><ul><li>Processes are modeled as deterministic extended finite state machines.</li><li>System actions can be performed by the node without requiring human interaction.</li></ul>"
+    "description": "<p>The Workflow Engine facilitates running processes and holding the state.</p><ul><li>Processes are modeled as deterministic extended finite state machines.</li><li>System actions can be performed by the node without requiring human interaction.</li></ul>"
   },
   {
     "title": "Event Chain Service",
-    "description": "<p>The Event chain service holds all ad hoc private blockchains an organization is involved in.</p><ul><li>One node holds many ad hoc blockchains. (similar to git repositories).</li><li>Fully decoupled.</li><li>Configurable allowing it to interact any microservices added to the application layer.</li></ul>"
+    "description": "<p>The Event Chain Service holds all ad hoc private blockchains an organization is involved in.</p><ul><li>One node holds many ad hoc blockchains. (similar to git repositories).</li><li>Fully decoupled.</li><li>Configurable allowing it to interact to any microservice added to the application layer.</li></ul>"
   },
   {
     "title": "Proof Engine",
-    "description": "<p>This node connects the LTO platform to the LTO Global Chain that is responsible for securing data and events through anchoring.</p><p>The LTO Global Chain is a fork of NXT. We have stripped everything that has to do with assets and tokens and added / changed the following:</p><ul><li>Use industry standard hashing algorithm (SHA2)</li><li>Replaced the custom Elliptic-curve with standard ED25519.</li><li>Added the Bitcoin NG protocol Daily summary blocks reduce size on disk and fasten set up time (not the same as ledger pruning).</li><li>New transaction types: Anchoring, certificates, SSI (DID protocol)</li></ul>"
+    "description": "<p>This node connects the LTO platform to the LTO Global Chain that is responsible for securing data and events through anchoring.</p><p>The LTO Global Chain is a fork of NXT. We have stripped everything that has to do with assets and tokens and added / changed the following:</p><ul><li>Use industry standard hashing algorithm (SHA2)</li><li>Replaced the custom Elliptic-curve with standard ED25519.</li><li>Added the Bitcoin NG protocol Daily summary blocks reduce size on disk and speed up set-up time (not the same as ledger pruning).</li><li>New transaction types: Anchoring, certificates, SSI (DID protocol)</li></ul>"
   },
   {
     "title": "API Gateway",
-    "description": "<p>API gateway is an http server which proxies all the requests to the right service within the node.</p>"
+    "description": "<p>The API Gateway is an HTTP server which proxies all the requests to the right service within the node.</p>"
   },
   {
     "title": "Queuer Service",
-    "description": "<p>The queuer service provides a rest api to queue events on the message queue.</p>"
+    "description": "<p>The Queuer Service provides a REST API to queue events on the Message Queue.</p>"
   },
   {
     "title": "Message Queue",
-    "description": "<p>Message queue is RabbitMQ message queue which uses shoveling to the distribute events to other nodes.</p>"
+    "description": "<p>Message Queue is RabbitMQ message queue which uses shoveling to distribute events to other nodes.</p>"
   },
   {
     "title": "Dispatcher Service",
-    "description": "<p>The dispatcher service parses messages from the message queue and pushes them to right service within the node.</p>"
+    "description": "<p>The Dispatcher Service parses messages from the message queue and pushes them to right service within the node.</p>"
   },
 ]
 
